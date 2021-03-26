8a095658bfeb5c1eaff0393e13273625b0ac5246
Alexander Vitshas <avitshas@gmail.com>
2021-03-26 15:40:32 +0300

    Функции добавления и удаления товара из корзины, изменение количества товаров Работает с API на node.js Информация о каталоге и корзине хранится в файлах server/db/catalogdata.json server/db/cartdata.json

---
+++ server/handler.js
@@ -0,0 +1,27 @@
+const cart = require("./cart");
+const fs = require("fs");
+
+const actions = {
+  add: cart.add,
+  change: cart.change,
+  delete: cart.del,
+};
+//HANDLER отвечает за изменение данных в самом файле
+let handler = async (req, res, action, file) => {
+  await fs.readFile(file, "utf-8", async (err, data) => {
+    if (err) {
+      res.sendStatus(404, JSON.stringify({ result: 0, text: err }));
+    } else {
+      let newCart = actions[action](JSON.parse(data), req);
+      await fs.writeFile(file, newCart, (err) => {
+        if (err) {
+          res.sendStatus(404, JSON.stringify({ result: 0, text: err }));
+        } else {
+          res.send(JSON.stringify({ result: 1 }));
+        }
+      });
+    }
+  });
+};
+
+module.exports = handler;
